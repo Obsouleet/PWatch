a34dade6a67f0753f714dfc38635b2a71923bb5a
Obsouleet <Obsouleet@users.noreply.github.com>
2015-05-05 20:37:49 +0200

    betterTap

--- src/feature_image.c
+++ src/feature_image.c
@@ -1,10 +1,17 @@
 #include "pebble.h"
  
 #define TAP_TIME 2000	
-#define EDIT_MODE_DURATION 3000	
+#define EDIT_MODE_DURATION 3000
+#define TRIPPLE_TAP_TIME 1100
+	
 static int director = 1; 
 static int colourcounter=0*4;  
 static int incer=0;
+static int ctr=0;
+static bool is_throttled=false;
+static bool editmode=false;
+
+
 static Window *s_main_window;
 
 static BitmapLayer *links_oben_layer;
@@ -12,14 +19,14 @@ static BitmapLayer *rechts_oben_layer;
 static BitmapLayer *bar_layer;
 static BitmapLayer *links_unten_layer;
 static BitmapLayer *rechts_unten_layer;
+
 static TextLayer *status_layer;
+
 static AppTimer *tap_timer = NULL;	
-static bool is_throttled;
-static bool editmode;
-static int ctr;
 static AppTimer *throttle_timer = NULL;	
 static AppTimer *enter_mode_timer = NULL;	
 static AppTimer *edit_mode_timer = NULL;	
+
 static GBitmap *IMAGE_0;
 static GBitmap *IMAGE_1;
 static GBitmap *IMAGE_2;
@@ -33,8 +40,9 @@ static GBitmap *IMAGE_9;
 static GBitmap *IMAGE_BAR;
 static GBitmap *V_IMAGE;
 
-
 static GColor Textcolour; 
+
+// Placeholders
 static void redaw_entire_screen();
 //  BLIT DIGIT ------------------------------------------------------------------------------------------------------------------------------
 
@@ -102,8 +110,7 @@ static void print_status(Window *window) {                                      
 		strcat(strcat(date_buffer, " * "),battery_buffer);
 
 		// Update time TextLayer
-		//redaw_entire_screen();
-	  text_layer_set_text_color(status_layer, Textcolour);
+		text_layer_set_text_color(status_layer, Textcolour);
 		text_layer_set_text(status_layer, date_buffer);
 }
 
@@ -116,7 +123,6 @@ static void timer_callback() {
 }
 
 static void reset_throttle() {
-//	app_timer_cancel(throttle_timer);
 	is_throttled=false;
 }
 
@@ -126,39 +132,24 @@ static void edit_mode_timer_reset() {
 }
 
 static void enter_mode_timer_reset() {
-
-	if(editmode==true) {
-			if(ctr==1) {
-					//next color
-					redaw_entire_screen();
-					if(app_timer_reschedule(edit_mode_timer, EDIT_MODE_DURATION)==false) {edit_mode_timer = app_timer_register(EDIT_MODE_DURATION, edit_mode_timer_reset, NULL);}									// keep editmode for 3 secs
-			}
-			if(ctr>1) {
-					// next colorblock
-					if(app_timer_reschedule(edit_mode_timer, EDIT_MODE_DURATION)==false) {edit_mode_timer = app_timer_register(EDIT_MODE_DURATION, edit_mode_timer_reset, NULL);}
-			}
-
-	} else {
+	if(editmode==false) {																					// trippleclick and outside editmode 
 			if(ctr>2) {
-				editmode=true;
-			//	redaw_entire_screen();
-			  text_layer_set_text_color(status_layer, Textcolour);
-				text_layer_set_text(status_layer, "Edit Mode");
 				app_timer_cancel(tap_timer);
-  			if(app_timer_reschedule(edit_mode_timer, EDIT_MODE_DURATION)==false) {edit_mode_timer = app_timer_register(EDIT_MODE_DURATION, edit_mode_timer_reset, NULL);}
+				editmode=true;
+				text_layer_set_text_color(status_layer, Textcolour);
+				text_layer_set_text(status_layer, "Edit Mode");
+				edit_mode_timer = app_timer_register(EDIT_MODE_DURATION, edit_mode_timer_reset, NULL);
 			}
-		
 	}
 ctr=0;	
 }
-
 
 
 // TAP-HANDLER -------------------------------------------------------------------------------------------------------------------------------
 
 static void tap_handler(AccelAxisType axis, int32_t direction) {
 
-if(is_throttled!=true) {
+if(is_throttled==false) {
 		is_throttled=true;
 		throttle_timer = app_timer_register(200, reset_throttle, NULL);
 	
@@ -181,23 +172,35 @@ if(is_throttled!=true) {
 			
 		case ACCEL_AXIS_Z:
 				if (direction > 0) {
-	
-						text_layer_set_text_color(status_layer, Textcolour);
+																// display date if not in edit mode
+						if(editmode==false){ 
+								if(app_timer_reschedule(tap_timer, TAP_TIME)==false) {
+									tap_timer = app_timer_register(TAP_TIME, timer_callback, NULL);														// reschedule TAPtimer else set em 				
+									text_layer_set_text_color(status_layer, Textcolour);
+									print_status(s_main_window);
+								}	
+								APP_LOG(APP_LOG_LEVEL_INFO, "normal single tap");
+																
+								if(ctr==0){
+										enter_mode_timer = app_timer_register(TRIPPLE_TAP_TIME, enter_mode_timer_reset, NULL);
+										ctr=1;
+										APP_LOG(APP_LOG_LEVEL_INFO, "ctr = 1");
+								} else {
+										ctr++;
+										APP_LOG(APP_LOG_LEVEL_INFO, "ctr ++");
+								}
+						} else {																												// editmode single tap
+									app_timer_cancel(tap_timer);
+									redaw_entire_screen();
+									text_layer_set_text(status_layer, "Edit Mode");
+									if(app_timer_reschedule(edit_mode_timer, EDIT_MODE_DURATION)==false) {
+										edit_mode_timer = app_timer_register(EDIT_MODE_DURATION, edit_mode_timer_reset, NULL);					// keep editmode for another 3 secs
+										editmode=true;
+										text_layer_set_text_color(status_layer, Textcolour);
+										text_layer_set_text(status_layer, "Edit Mode");				
+									}									
+						}
 
-						if(editmode==false) { 
-								if(app_timer_reschedule(tap_timer, TAP_TIME)==false) {tap_timer = app_timer_register(TAP_TIME, timer_callback, NULL);}														// reschedule TAPtimer else set em 				
-								print_status(s_main_window);
-								APP_LOG(APP_LOG_LEVEL_INFO, "Z axis positive.");
-						} 
-				
-						if(ctr==0){
-								enter_mode_timer = app_timer_register(1100, enter_mode_timer_reset, NULL);
-								ctr=1;
-		   			} else {
-						  	ctr++;
-				  	}
-					
-					
 				} else {
 					APP_LOG(APP_LOG_LEVEL_INFO, "Z axis negative.");
 				}
@@ -237,9 +240,8 @@ static void update_time() {                                                     
   } else {
             // NEUE MINUTE
     
-           if((minute % 10) == 0) {
-             blit_digit(links_unten_layer, minute / 10);}         											// Minute1
-           	blit_digit(rechts_unten_layer, (minute % 10));                              // Minute2
+           if((minute % 10) == 0) {blit_digit(links_unten_layer, minute / 10);}        // Minute1
+           blit_digit(rechts_unten_layer, (minute % 10));                              // Minute2
   }
 }
 
@@ -289,12 +291,11 @@ static void main_window_load(Window *window) {
   // Status Text Layer
   status_layer = text_layer_create(GRect(7, 76, 130, 17));
   text_layer_set_background_color(status_layer, GColorClear);
-  //text_layer_set_text_color(status_layer, GColorBlack);
   text_layer_set_font(status_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
   text_layer_set_text_alignment(status_layer, GTextAlignmentCenter);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(status_layer));
 	
-	redaw_entire_screen();
+  redaw_entire_screen();
 	
 }
  
@@ -304,7 +305,7 @@ static void redaw_entire_screen(){
 	
 	// All Images
 	
-	GColor *current_palette0 = gbitmap_get_palette(IMAGE_0);
+  GColor *current_palette0 = gbitmap_get_palette(IMAGE_0);
   GColor *current_palette1 = gbitmap_get_palette(IMAGE_1);
   GColor *current_palette2 = gbitmap_get_palette(IMAGE_2);
   GColor *current_palette3 = gbitmap_get_palette(IMAGE_3);
@@ -319,24 +320,50 @@ static void redaw_entire_screen(){
 
 		 //  Color Sets
 
-	GColor colours[]={GColorBlueMoon,	GColorPictonBlue,	GColorCeleste,GColorWhite,
-                    GColorWindsorTan,	GColorRajah,	GColorIcterine,	GColorPastelYellow,		
-                    GColorDarkGreen,	GColorMayGreen,	GColorMelon,	GColorWhite,		
-                    GColorBlack,	GColorDarkGray,	GColorLightGray,	GColorWhite,		
-                    GColorIslamicGreen,	GColorScreaminGreen,	GColorMelon,	GColorWhite,		
-                    GColorDukeBlue,	GColorVeryLightBlue,	GColorBabyBlueEyes,	GColorWhite,		
-                    GColorArmyGreen,	GColorBrass,	GColorPastelYellow,	GColorWhite,		
-                    GColorBulgarianRose,	GColorRoseVale,	GColorMelon,	GColorWhite,		
-                    GColorImperialPurple,	GColorPurpureus,	GColorRichBrilliantLavender,	GColorWhite,		
-                    GColorMidnightGreen,	GColorCadetBlue,	GColorCeleste,	GColorWhite,		
-                    GColorOxfordBlue,	GColorLiberty,	GColorBabyBlueEyes,	GColorWhite,		
-                    GColorDarkCandyAppleRed,	GColorSunsetOrange,	GColorMelon,	GColorWhite,		
-                    GColorWindsorTan,	GColorRajah,	GColorPastelYellow,	GColorWhite,		
-                    GColorGreen,	GColorScreaminGreen,	GColorMelon,	GColorWhite};
-			
-  //char buffer[20];
-	//APP_LOG(APP_LOG_LEVEL_INFO, "Entering redraw...");
-	
+GColor colours[]={	GColorBlueMoon,	GColorPictonBlue,	GColorCeleste,GColorWhite,
+					GColorWindsorTan,	GColorRajah,	GColorIcterine,	GColorPastelYellow,		
+					GColorDarkGreen,	GColorMayGreen,	GColorMelon,	GColorWhite,		
+					GColorBlack,	GColorDarkGray,	GColorLightGray,	GColorWhite,		
+					GColorIslamicGreen,	GColorScreaminGreen,	GColorMelon,	GColorWhite,		
+					GColorDukeBlue,	GColorVeryLightBlue,	GColorBabyBlueEyes,	GColorWhite,		
+					GColorArmyGreen,	GColorBrass,	GColorPastelYellow,	GColorWhite,		
+					GColorBulgarianRose,	GColorRoseVale,	GColorMelon,	GColorWhite,		
+					GColorImperialPurple,	GColorPurpureus,	GColorRichBrilliantLavender,	GColorWhite,		
+					GColorMidnightGreen,	GColorCadetBlue,	GColorCeleste,	GColorWhite,		
+					GColorOxfordBlue,	GColorLiberty,	GColorBabyBlueEyes,	GColorWhite,		
+					GColorDarkCandyAppleRed,	GColorSunsetOrange,	GColorMelon,	GColorWhite,		
+					GColorWindsorTan,	GColorRajah,	GColorPastelYellow,	GColorWhite,		
+					GColorGreen,	GColorScreaminGreen,	GColorMelon,	GColorWhite	};
+
+GColor reds[]={		GColorBulgarianRose, GColorRoseVale, GColorMelon, GColorWhite, 
+					GColorImperialPurple, GColorPurpureus, GColorRichBrilliantLavender, GColorWhite, 
+					GColorDarkCandyAppleRed, GColorSunsetOrange, GColorMelon, GColorWhite, 
+					GColorDarkCandyAppleRed, GColorBulgarianRose, GColorBulgarianRose, GColorBlack, 
+					GColorRed, GColorDarkCandyAppleRed, GColorBulgarianRose, GColorBlack	};
+  
+GColor blues[]={	GColorBlueMoon, GColorPictonBlue, GColorCeleste, GColorWhite, 
+					GColorDukeBlue, GColorVeryLightBlue, GColorBabyBlueEyes, GColorWhite, 
+					GColorMidnightGreen, GColorCadetBlue, GColorCeleste, GColorWhite, 
+					GColorOxfordBlue, GColorLiberty, GColorBabyBlueEyes, GColorWhite, 
+					GColorBlueMoon, GColorDukeBlue, GColorOxfordBlue, GColorBlack, 
+					GColorBlueMoon, GColorDukeBlue, GColorOxfordBlue, GColorBlack};
+					
+GColor greens[]={	GColorDarkGreen, GColorMayGreen, GColorMelon, GColorWhite, 
+					GColorIslamicGreen, GColorScreaminGreen, GColorMelon, GColorWhite, 
+					GColorArmyGreen, GColorBrass, GColorPastelYellow, GColorWhite, 
+					GColorGreen, GColorScreaminGreen, GColorMelon, GColorWhite, 
+					GColorGreen, GColorIslamicGreen, GColorDarkGreen, GColorBlack, 
+					GColorMediumSpringGreen, GColorJaegerGreen, GColorDarkGreen, GColorBlack};
+
+GColor others[]={	GColorWindsorTan, GColorRajah, GColorIcterine, GColorPastelYellow, 
+					GColorWindsorTan, GColorRajah, GColorPastelYellow, GColorWhite, 
+					GColorWindsorTan, GColorArmyGreen, GColorBulgarianRose, GColorBlack, 
+					GColorYellow, GColorLimerick, GColorArmyGreen, GColorBlack, 
+					GColorYellow, GColorIcterine, GColorPastelYellow, GColorWhite, 
+					GColorSunsetOrange, GColorMelon, GColorCeleste, GColorElectricBlue, 
+					GColorMediumSpringGreen, GColorScreaminGreen, GColorSpringBud, GColorYellow};
+  
+ 	
 	for(int pals=0;pals<11;pals++){                                                                                                      // walk through all images of the project
 
 			incer=0; 
@@ -344,28 +371,28 @@ static void redaw_entire_screen(){
 			for(int i=(colourcounter+(3*director));i!=(colourcounter+abs((4*director)-4)-director);i=(i+((director-1+director))*-1)) {         // up- or down the colour array 
 				Paletten[pals][incer]=colours[i];                                                                                         
 				incer++;
-				}
-  }  
-  if (director==1){
-			 director = 0;                                       // change direction
-			 colourcounter=colourcounter + 4;                    // next colourset
-			 if(colourcounter>55){colourcounter=0;}              // reset Colourcycle
-  }  else {
-	     director = 1;                                       // change direction
-  }
- 
+			}
+	}  
+	if (director==1){
+				director = 0;                                       // change direction
+				colourcounter=colourcounter + 4;                    // next colourset
+				if(colourcounter>55){colourcounter=0;}              // reset Colourcycle
+	}  else {
+				director = 1;                                       // change direction
+	}
+	 
 	time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  int minute = tick_time->tm_min;
-  int hour = tick_time->tm_hour;
-	
+	struct tm *tick_time = localtime(&temp);
+	int minute = tick_time->tm_min;
+	int hour = tick_time->tm_hour;
+		
 	blit_digit(links_oben_layer, hour / 10 );                   
-  blit_digit(rechts_oben_layer, hour % 10 );
-  blit_digit(links_unten_layer, minute / 10);                             
-  blit_digit(rechts_unten_layer, minute % 10);  
+	blit_digit(rechts_oben_layer, hour % 10 );
+	blit_digit(links_unten_layer, minute / 10);                             
+	blit_digit(rechts_unten_layer, minute % 10);  
 	window_set_background_color(s_main_window, Paletten[0][0]);
 	Textcolour=Paletten[0][0];
-	//APP_LOG(APP_LOG_LEVEL_INFO, buffer);
+
 }
 
 // SYSTEM RELATED -----------------------------------------------------------------------------------------------------------------------
