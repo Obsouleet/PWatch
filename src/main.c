2e6c1c36c74ee6fdf96ab5969afbb9a18fde1ec4
Obsouleet <Obsouleet@users.noreply.github.com>
2015-12-07 19:47:41 +0100

    Final - time to fork

--- src/main.c
+++ src/main.c
@@ -21,10 +21,13 @@
 #define KEY_COL31 14
 #define KEY_COL32 15
 
+// #define ALTFONT 1
+ #define NEUFONT 1
+
 static int incer =0 ;
 static bool twelvehr = false;
 static bool showdate =false;
-
+static bool altfont = true;
 static GColor col0;
 static GColor col1;
 static GColor col2;
@@ -40,6 +43,10 @@ static BitmapLayer *links_unten_layer;
 static BitmapLayer *rechts_unten_layer;
 
 static TextLayer *status_layer;
+static GBitmap *IMAGE_BARNULL;
+
+#if defined(ALTFONT)
+
 static GBitmap *IMAGE_0;
 static GBitmap *IMAGE_1;
 static GBitmap *IMAGE_2;
@@ -51,7 +58,22 @@ static GBitmap *IMAGE_7;
 static GBitmap *IMAGE_8;
 static GBitmap *IMAGE_9;
 static GBitmap *IMAGE_BAR;
-static GBitmap *IMAGE_BARNULL;
+
+#elif defined(NEUFONT)
+
+static GBitmap *IMAGE_0_alt;
+static GBitmap *IMAGE_1_alt;
+static GBitmap *IMAGE_2_alt;
+static GBitmap *IMAGE_3_alt;
+static GBitmap *IMAGE_4_alt;
+static GBitmap *IMAGE_5_alt;
+static GBitmap *IMAGE_6_alt;
+static GBitmap *IMAGE_7_alt;
+static GBitmap *IMAGE_8_alt;
+static GBitmap *IMAGE_9_alt;
+static GBitmap *IMAGE_BAR_alt;
+
+#endif
 
 static GBitmap *V_IMAGE;
 
@@ -155,8 +177,50 @@ static void inbox_received_handler(DictionaryIterator *iter, void *context) {
 }
 
 static void blit_digit(void *bitmap_layer, int image) {
-                switch (image)
 
+#if defined(NEUFONT)  
+		switch (image)
+								{
+                               case 0:
+                                               V_IMAGE=IMAGE_0_alt;
+                                              break;
+                               case 1:
+                                               V_IMAGE=IMAGE_1_alt;
+                                              break;
+                               case 2:
+                                               V_IMAGE=IMAGE_2_alt;
+                                              break;
+                               case 3:
+                                               V_IMAGE=IMAGE_3_alt;
+                                              break;
+                               case 4:
+                                               V_IMAGE=IMAGE_4_alt;
+                                              break;
+                               case 5:
+                                               V_IMAGE=IMAGE_5_alt;
+                                              break;
+                               case 6:
+                                               V_IMAGE=IMAGE_6_alt;
+                                              break;
+                               case 7:
+                                               V_IMAGE=IMAGE_7_alt;
+                                              break;
+                               case 8:
+                                               V_IMAGE=IMAGE_8_alt;
+                                              break;
+                               case 9:
+                                               V_IMAGE=IMAGE_9_alt;
+                                              break;
+                               case 10:
+                                               V_IMAGE=IMAGE_BAR_alt;
+																							 break;
+									 						 case 11:
+                                               V_IMAGE=IMAGE_BARNULL;
+                }
+
+#elif defined(ALTFONT)
+	
+		switch (image)
 								{
                                case 0:
                                                V_IMAGE=IMAGE_0;
@@ -194,7 +258,7 @@ static void blit_digit(void *bitmap_layer, int image) {
 									 						 case 11:
                                                V_IMAGE=IMAGE_BARNULL;
                 }
-
+#endif
 							bitmap_layer_set_bitmap(bitmap_layer, V_IMAGE);
 }
 
@@ -220,7 +284,7 @@ static void battery_handler(BatteryChargeState charge_state)  {                 
 			  const	char *current_locale =setlocale(LC_ALL, "");
 
 	 			if (twelvehr == false && strcmp("en_US", current_locale) == 0) {					// Pebble only knows en_US - typical...
-									date_format= "%a %d/%m/%Y";																			// if not 12 hour format and english this must be britain 
+									date_format= "%a %d/%m/%y";																			// if not 12 hour format and english this must be britain 
 				}
 	 
 				strftime(date_buffer, 22, date_format, tick_time);
@@ -242,13 +306,11 @@ void handle_bluetooth( bool connected ) {
 	if ( prev_bt_status != connected ) {vibes_double_pulse();}
 	
 	if ( connected ) {
-		
-    bitmap_layer_set_bitmap(bar_layer, IMAGE_BAR);
+		blit_digit(bar_layer, 10);       
 		Textcolour=col0;
 
 	} else {
-
-    bitmap_layer_set_bitmap(bar_layer, IMAGE_BARNULL);	
+		blit_digit(bar_layer, 11);       
 		Textcolour=col3;
 	}
 
@@ -301,6 +363,7 @@ static void main_window_load(Window *window) {
  
   /*   Define images                         */
  
+	#if defined(ALTFONT)  
   IMAGE_0 = gbitmap_create_with_resource(RESOURCE_ID_digit_0);
   IMAGE_1 = gbitmap_create_with_resource(RESOURCE_ID_digit_1);
   IMAGE_2 = gbitmap_create_with_resource(RESOURCE_ID_digit_2);
@@ -312,29 +375,55 @@ static void main_window_load(Window *window) {
   IMAGE_8 = gbitmap_create_with_resource(RESOURCE_ID_digit_8);
   IMAGE_9 = gbitmap_create_with_resource(RESOURCE_ID_digit_9);
   IMAGE_BAR = gbitmap_create_with_resource(RESOURCE_ID_bar);
+	#elif defined(NEUFONT)  
+	IMAGE_0_alt = gbitmap_create_with_resource(RESOURCE_ID_image_0_alt);
+  IMAGE_1_alt = gbitmap_create_with_resource(RESOURCE_ID_image_1_alt);
+  IMAGE_2_alt = gbitmap_create_with_resource(RESOURCE_ID_image_2_alt);
+  IMAGE_3_alt = gbitmap_create_with_resource(RESOURCE_ID_image_3_alt);
+  IMAGE_4_alt = gbitmap_create_with_resource(RESOURCE_ID_image_4_alt);
+  IMAGE_5_alt = gbitmap_create_with_resource(RESOURCE_ID_image_5_alt);
+  IMAGE_6_alt = gbitmap_create_with_resource(RESOURCE_ID_image_6_alt);
+  IMAGE_7_alt = gbitmap_create_with_resource(RESOURCE_ID_image_7_alt);
+  IMAGE_8_alt = gbitmap_create_with_resource(RESOURCE_ID_image_8_alt);
+  IMAGE_9_alt = gbitmap_create_with_resource(RESOURCE_ID_image_9_alt);
+  IMAGE_BAR_alt = gbitmap_create_with_resource(RESOURCE_ID_image_bar_alt);
+	#endif
 	IMAGE_BARNULL = gbitmap_create_with_resource(RESOURCE_ID_BARNULL);
 	
   /* Define graphic areas              */
  
-  links_oben_layer = bitmap_layer_create(GRect(4, 4, 66, 66));
+	#if defined(PBL_RECT)
+  int xadd=0;
+	int yadd=0;
+	#elif defined(PBL_ROUND)
+  int xadd=18;
+	int yadd=5;
+	#endif
+	
+  links_oben_layer = bitmap_layer_create(GRect(4+xadd, 4+yadd, 66, 66));
   layer_add_child(window_layer, bitmap_layer_get_layer(links_oben_layer));
   
-  rechts_oben_layer = bitmap_layer_create(GRect(74, 4, 66, 66));
+  rechts_oben_layer = bitmap_layer_create(GRect(74+xadd, 4+yadd, 66, 66));
   layer_add_child(window_layer, bitmap_layer_get_layer(rechts_oben_layer));
   
-  links_unten_layer = bitmap_layer_create(GRect(4, 99, 66, 66));
+  links_unten_layer = bitmap_layer_create(GRect(4+xadd, 99+yadd, 66, 66));
   layer_add_child(window_layer, bitmap_layer_get_layer(links_unten_layer));
   
-  rechts_unten_layer = bitmap_layer_create(GRect(74, 99, 66, 66));
+  rechts_unten_layer = bitmap_layer_create(GRect(74+xadd, 99+yadd, 66, 66));
   layer_add_child(window_layer, bitmap_layer_get_layer(rechts_unten_layer));
  
-  bar_layer = bitmap_layer_create(GRect(4, 74, 136, 22));
+  bar_layer = bitmap_layer_create(GRect(4+xadd, 74+yadd, 136, 22));
+	
+	#if defined(NEUFONT)  
+  bitmap_layer_set_bitmap(bar_layer, IMAGE_BAR_alt);
+	#elif defined(ALTFONT)  
   bitmap_layer_set_bitmap(bar_layer, IMAGE_BAR);
+	#endif
   layer_add_child(window_layer, bitmap_layer_get_layer(bar_layer));
  
   // Status Text Layer
 	
-  status_layer = text_layer_create(GRect(4, 72, 139, 19));
+  status_layer = text_layer_create(GRect(4+xadd, 72+yadd, 139, 19));
   text_layer_set_background_color(status_layer, GColorClear);
   text_layer_set_font(status_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   text_layer_set_text_alignment(status_layer, GTextAlignmentCenter);
@@ -349,7 +438,11 @@ static void redaw_entire_screen(){
 
 	// All Images
 	
-  GColor *current_palette0 = gbitmap_get_palette(IMAGE_0);
+	GColor *current_palettebarnull = gbitmap_get_palette(IMAGE_BARNULL);
+	
+	#if defined(ALTFONT) 
+
+	GColor *current_palette0 = gbitmap_get_palette(IMAGE_0);
   GColor *current_palette1 = gbitmap_get_palette(IMAGE_1);
   GColor *current_palette2 = gbitmap_get_palette(IMAGE_2);
   GColor *current_palette3 = gbitmap_get_palette(IMAGE_3);
@@ -360,9 +453,24 @@ static void redaw_entire_screen(){
   GColor *current_palette8 = gbitmap_get_palette(IMAGE_8);
   GColor *current_palette9 = gbitmap_get_palette(IMAGE_9);
   GColor *current_palettebar = gbitmap_get_palette(IMAGE_BAR);
-	GColor *current_palettebarnull = gbitmap_get_palette(IMAGE_BARNULL);
+	GColor *Paletten[]={current_palette0,current_palette1, current_palette2,current_palette3,current_palette4,current_palette5, current_palette6,current_palette7,current_palette8,current_palette9,current_palettebar,current_palettebarnull};
 	
-  GColor *Paletten[]={current_palette0,current_palette1, current_palette2,current_palette3,current_palette4,current_palette5, current_palette6,current_palette7,current_palette8,current_palette9,current_palettebar,current_palettebarnull};
+	#elif defined(NEUFONT) 
+	GColor *current_palette0_alt = gbitmap_get_palette(IMAGE_0_alt);
+  GColor *current_palette1_alt = gbitmap_get_palette(IMAGE_1_alt);
+  GColor *current_palette2_alt = gbitmap_get_palette(IMAGE_2_alt);
+  GColor *current_palette3_alt = gbitmap_get_palette(IMAGE_3_alt);
+  GColor *current_palette4_alt = gbitmap_get_palette(IMAGE_4_alt);
+  GColor *current_palette5_alt = gbitmap_get_palette(IMAGE_5_alt);
+  GColor *current_palette6_alt = gbitmap_get_palette(IMAGE_6_alt);
+  GColor *current_palette7_alt = gbitmap_get_palette(IMAGE_7_alt);
+  GColor *current_palette8_alt = gbitmap_get_palette(IMAGE_8_alt);
+  GColor *current_palette9_alt = gbitmap_get_palette(IMAGE_9_alt);
+  GColor *current_palettebar_alt = gbitmap_get_palette(IMAGE_BAR_alt);
+  GColor *Paletten[]={current_palette0_alt,current_palette1_alt, current_palette2_alt,current_palette3_alt,current_palette4_alt,current_palette5_alt, current_palette6_alt,current_palette7_alt,current_palette8_alt,current_palette9_alt,current_palettebar_alt,current_palettebarnull};
+	#endif
+	
+	
 	GColor Farben[]={col0, col1, col2, col3};
 
 	for(int pals=0; pals<12;pals++){
@@ -388,7 +496,7 @@ static void redaw_entire_screen(){
 	Textcolour=Paletten[0][0];
 
 																																							//Datum anzeigen	
-		handle_bluetooth(bluetooth_connection_service_peek());										// calls battery_handler(battery_state_service_peek());												
+	handle_bluetooth(bluetooth_connection_service_peek());										// calls battery_handler(battery_state_service_peek());												
 									
 }
 
@@ -401,6 +509,8 @@ static void main_window_unload(Window *window) {
 		bitmap_layer_destroy(rechts_unten_layer);
 		bitmap_layer_destroy(bar_layer);
 		text_layer_destroy(status_layer);
+	
+	#if defined(ALTFONT) 	
 		gbitmap_destroy(IMAGE_0);
 		gbitmap_destroy(IMAGE_1);
 		gbitmap_destroy(IMAGE_2);
@@ -412,6 +522,22 @@ static void main_window_unload(Window *window) {
 		gbitmap_destroy(IMAGE_8);
 		gbitmap_destroy(IMAGE_9);
 		gbitmap_destroy(IMAGE_BAR);
+
+	#elif defined(NEUFONT) 
+		gbitmap_destroy(IMAGE_0_alt);
+		gbitmap_destroy(IMAGE_1_alt);
+		gbitmap_destroy(IMAGE_2_alt);
+		gbitmap_destroy(IMAGE_3_alt);
+		gbitmap_destroy(IMAGE_4_alt);
+		gbitmap_destroy(IMAGE_5_alt);
+		gbitmap_destroy(IMAGE_6_alt);
+		gbitmap_destroy(IMAGE_7_alt);
+		gbitmap_destroy(IMAGE_8_alt);
+		gbitmap_destroy(IMAGE_9_alt);
+		gbitmap_destroy(IMAGE_BAR_alt);
+	#endif
+		gbitmap_destroy(IMAGE_BARNULL);
+	
 }
 
 static void init() {
@@ -464,7 +590,7 @@ static void deinit() {
 	battery_state_service_unsubscribe();
 	tick_timer_service_unsubscribe();
 	bluetooth_connection_service_unsubscribe();
-
+	app_message_deregister_callbacks();
 }
  
 int main(void) {
